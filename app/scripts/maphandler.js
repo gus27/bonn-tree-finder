b2128aa41ff82159a7cff25579ce3c5fb1cc1c45
gus27 <gus-github@guxx.net>
2017-07-14 23:55:00 +0200

    change http to https for accessing Google's chart API.

--- app/scripts/maphandler.js
+++ app/scripts/maphandler.js
@@ -263,7 +263,7 @@ function MapHandler() {
         }
 
         var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor + '|24|_|'+text,
+            'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor + '|24|_|'+text,
             new google.maps.Size(21, 34),
             new google.maps.Point(0, 0),
             new google.maps.Point(10, 34),
